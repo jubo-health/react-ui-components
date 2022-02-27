2c9a588789c7e0e45c3e3e9c355b4b37866b0bf6
Hengry <hengry@jubo.health>
2022-02-27 20:48:28 +0800

    animation version

--- src/HideOnScroll/HideOnScroll.stories.tsx
+++ src/HideOnScroll/HideOnScroll.stories.tsx
@@ -10,29 +10,50 @@ export default {
 } as Meta;
 
 export const PlayGround: Story<HideOnScrollProps> = (args) => {
-  const lastTime = React.useRef(+new Date());
   const lastY = React.useRef(0);
   const ref = React.useRef<HTMLDivElement>(null);
-  const [show, setShow] = React.useState(false);
+  const anchor = React.useRef(0);
+
   return (
     <div
       className="h-80 relative overflow-auto"
       onScroll={(e: React.UIEvent<HTMLElement>) => {
-        const speed =
-          (e.currentTarget.scrollTop - lastY.current) /
-          (e.timeStamp - lastTime.current);
-        if (speed > 3 && show) setShow(false);
-        else if (speed < -3) setShow(true);
-        lastTime.current = e.timeStamp;
-        lastY.current = e.currentTarget.scrollTop;
+        if (ref.current) {
+          const hide = ref.current.style.animationDelay === '-1s';
+          const dy = e.currentTarget.scrollTop - lastY.current;
+          const dh = e.currentTarget.scrollTop - anchor.current;
+
+          if ((!hide && dy > 0) || dh < ref.current.offsetHeight) {
+            const moveOutPropotion = dh / ref.current.offsetHeight;
+            if (moveOutPropotion < 1) {
+              ref.current.style.animationDelay = `-${moveOutPropotion}s`;
+            } else {
+              ref.current.style.animationDelay = '-1s';
+              anchor.current = 0;
+            }
+          }
+          if (dy < 0) {
+            const show = ref.current.style.animationDelay === '0s';
+            if (show) {
+              anchor.current = e.currentTarget.scrollTop;
+            }
+            if (dy < -80) {
+              ref.current.style.animationDelay = '0s';
+            }
+          } else if (
+            e.currentTarget.scrollTop + e.currentTarget.clientHeight ===
+            e.currentTarget.scrollHeight
+          ) {
+            ref.current.style.animationDelay = '0s';
+          }
+          lastY.current = e.currentTarget.scrollTop;
+        }
       }}
     >
-      <div className="sticky top-0 h-8 bg-white z-10">fixed</div>
+      {/* <div className="sticky top-0 h-8 bg-white z-10">fixed</div> */}
       <HideOnScroll
-        className={clsx(
-          'transition-top ease-linear duration-75 sticky top-0',
-          show && 'top-8'
-        )}
+        className={clsx('animate-zjit-hacked sticky top-0 animate-scroll-out')}
+        ref={ref}
         {...args}
       >
         Hide
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
       transitionProperty: {
         top: 'top',
       },
+      animation: {
+        'zjit-hacked': 'scroll-out 1s',
+        'scroll-out':
+          '1.0001s linear 0s infinite normal both paused scroll-out',
+      },
+      keyframes: {
+        'scroll-out': {
+          '100%': { transform: 'translate(0, -100%)' },
+        },
+      },
     },
   },
   variants: {
