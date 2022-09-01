b210fbc2b2463f4124fcd322c0ac3f11254093b4
Hengry <hengry@jubo.health>
2022-09-01 11:08:46 +0800

    export NumberInput

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import Bar from './Bar';
+import NumberInput from './NumberInput';
 import TextInput from './TextInput';
 import Textarea from './Textarea';
 import TransitionScroller from './TransitionScroller';
 
-export { Bar, TextInput, Textarea, TransitionScroller };
+export { Bar, NumberInput, TextInput, Textarea, TransitionScroller };
