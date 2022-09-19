3441a817fe23efee98ebf30ffba62955f9adea03
Hengry <hengry@jubo.health>
2022-09-19 16:01:23 +0800

    add Icon template

--- src/icons/LoadingIcon.tsx
+++ src/icons/LoadingIcon.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 
 import './style.css';
 
-const LoadingIcon = props => {
-  return (
-    <svg className='circular' height='24px' width='24px'>
-      <circle
-        className='path stroke-primary'
-        cx='12'
-        cy='12'
-        r='9'
-        fill='none'
-        strokeWidth='2'
-        strokeMiterlimit='10'
-      />
-    </svg>
-  );
-};
+const LoadingIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg className='circular' height='24px' width='24px' {...props}>
+    <circle
+      className='path stroke-primary'
+      cx='12'
+      cy='12'
+      r='9'
+      fill='none'
+      strokeWidth='2'
+      strokeMiterlimit='10'
+    />
+  </svg>
+);
 
 export default LoadingIcon;
