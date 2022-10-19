7236b975e458cae3f3fe72894e31d633fa031b29
Hengry <hengry@jubo.health>
2022-10-19 15:45:40 +0800

    enable testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     {
       name: '@storybook/addon-postcss',
       options: {
@@ -14,10 +15,13 @@ module.exports = {
       },
     },
   ],
+  features: {
+    interactionsDebugger: true, // 👈 Enable playback controls
+  },
   core: {
     builder: 'webpack5',
   },
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
