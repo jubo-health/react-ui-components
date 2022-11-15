74e61cf9226d2338c36125b9601ef5a2cc869265
Hengry <hengry@jubo.health>
2022-11-15 19:21:15 +0800

    export Label

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { default as TextInput } from './TextInput';
 export { default as NumberInput } from './NumberInput';
 export { default as Textarea } from './Textarea';
 export { default as Button } from './Button';
+export { default as Label } from './Label';
