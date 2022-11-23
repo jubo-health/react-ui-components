d8970b2492efbb150aae32c99e506eb1d379f757
Hengry <hengry@jubo.health>
2022-11-23 18:46:07 +0800

    FormButton: remove redundant props

--- src/FormButton/FormButton.tsx
+++ src/FormButton/FormButton.tsx
@@ -6,59 +6,50 @@ import CheckIcon from '../icons/CheckIcon';
 
 export interface FormButtonProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
-  /**
-   * 文字與間距大小，通常表單內使用lg，表單外使用sm
-   * (需注意此屬性與原生的重複，原生的size更名為widthInCharLength)
-   */
-  size?: 'sm' | 'lg';
   name: string;
   value: string;
   selected?: boolean;
   color?: 'primary' | 'secondary';
 }
 
-const defaultProps = { size: 'lg' } as FormButtonProps;
-
-const FormButton = React.forwardRef<
-  HTMLInputElement,
-  FormButtonProps & typeof defaultProps
->((props, ref) => {
-  const { name, size, children, className, value, color, ...rest } = props;
-  const id = React.useMemo(() => uniqueId('form-button-'), []);
-  return (
-    <div className='relative'>
-      <input
-        id={id}
-        value={value}
-        name={name}
-        className='peer hidden'
-        ref={ref}
-        type='checkbox'
-        {...rest}
-      />
-      <CheckIcon className='fill-primary invisible pointer-events-none peer-checked:visible absolute m-2' />
-      <div
-        className={twMerge(
-          'absolute pointer-events-none border rounded-sm border-grey-700 peer-checked:border-primary w-full h-full',
-          color === 'secondary' && 'border-grey-400'
-        )}
-      />
-      <label
-        htmlFor={id}
-        className={twMerge(
-          'flex select-none rounded-sm w-full h-full',
-          'text-lg leading-6 p-1',
-          'text-grey-700 peer-checked:text-primary',
-          color === 'secondary' && 'text-grey-400',
-          className
-        )}
-      >
-        <CheckIcon className='invisible m-1' />
-        <div className='m-1'>{children}</div>
-      </label>
-    </div>
-  );
-});
-FormButton.defaultProps = defaultProps;
+const FormButton = React.forwardRef<HTMLInputElement, FormButtonProps>(
+  (props, ref) => {
+    const { name, children, className, value, color, ...rest } = props;
+    const id = React.useMemo(() => uniqueId('form-button-'), []);
+    return (
+      <div className='relative'>
+        <input
+          id={id}
+          value={value}
+          name={name}
+          className='peer hidden'
+          ref={ref}
+          type='checkbox'
+          {...rest}
+        />
+        <CheckIcon className='fill-primary invisible pointer-events-none peer-checked:visible absolute m-2' />
+        <div
+          className={twMerge(
+            'absolute pointer-events-none border rounded-sm border-grey-700 peer-checked:border-primary w-full h-full',
+            color === 'secondary' && 'border-grey-400'
+          )}
+        />
+        <label
+          htmlFor={id}
+          className={twMerge(
+            'flex select-none rounded-sm w-full h-full',
+            'text-lg leading-6 p-1',
+            'text-grey-700 peer-checked:text-primary',
+            color === 'secondary' && 'text-grey-400',
+            className
+          )}
+        >
+          <CheckIcon className='invisible m-1' />
+          <div className='m-1'>{children}</div>
+        </label>
+      </div>
+    );
+  }
+);
 
 export default FormButton;
