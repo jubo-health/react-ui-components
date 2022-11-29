2630f81162a37a2f477d36d98cb5808dc7e80ba1
Hengry <hengry@jubo.health>
2022-11-29 11:14:15 +0800

    Checkbox & Radio using mutual base: CheckboxBase

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -1,101 +1,32 @@
 import React from 'react';
-import { twMerge } from 'tailwind-merge';
-import uniqueId from 'lodash/uniqueId';
 
-import FormButton from '../FormButton';
-import { PropsOf, AsProps, MutuallyExclude } from '../types';
+import CheckboxBase, { CheckboxBaseProps } from '../base/CheckboxBase';
 
-interface MutualStates {
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  value: string[];
-  name?: string;
-}
-
-const Context = React.createContext<MutualStates>({ value: [] });
-
-export type FieldInputProps<BaseElement> = AsProps<BaseElement> &
-  Omit<PropsOf<BaseElement>, 'type' | 'onChange' | 'name'>;
-
-const DEFAULT_BASE = FormButton;
-function CheckboxOption<
-  BaseElement extends React.ElementType = typeof DEFAULT_BASE
->(props: FieldInputProps<BaseElement>) {
-  const { as, value: propsValue, ...rest } = props;
-  const { onChange, value, name } = React.useContext(Context);
-  return React.createElement(as || DEFAULT_BASE, {
-    onChange,
-    checked: value.includes(propsValue),
-    value: propsValue,
-    name,
-    ...rest,
-  });
-}
-
-interface Option {
-  value: string;
-  label: string;
-  sublabel?: string;
-}
-
-interface CheckboxProps {
-  options: Option[];
-  children: React.ReactNode;
+type CheckboxProps = CheckboxBaseProps<string[]> & {
   onChange?: (
     state: string[],
-    event?:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.MouseEvent<HTMLInputElement>
+    event?: React.ChangeEvent<HTMLInputElement>
   ) => void;
-  value: string[];
-  name?: string;
-  className?: string;
-}
-const defaultProps = {
-  name: uniqueId('radio-'),
-} as CheckboxProps;
-function Checkbox(
-  props: MutuallyExclude<
-    CheckboxProps & typeof defaultProps,
-    'options' | 'children'
-  >
-) {
-  const {
-    options,
-    value = [],
-    onChange,
-    children,
-    name = '',
-    className,
-  } = props;
-  const context = React.useMemo(
-    () => ({
-      name,
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        const index = value.findIndex(v => v === e.target.value);
-        if (onChange)
-          onChange(
-            index > -1
-              ? [...value.slice(0, index), ...value.slice(index + 1)]
-              : [...value, e.target.value],
-            e
-          );
-      },
-      value,
-    }),
-    [name, onChange, value]
+};
+
+function Checkbox(props: CheckboxProps) {
+  const { value = [], onChange, name = '', ...rest } = props;
+  const handleChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const index = value.findIndex(v => v === e.target.value);
+      if (onChange)
+        onChange(
+          index > -1
+            ? [...value.slice(0, index), ...value.slice(index + 1)]
+            : [...value, e.target.value],
+          e
+        );
+    },
+    [onChange, value]
   );
   return (
-    <Context.Provider value={context}>
-      <div className={twMerge('grid grid-cols-2 gap-2', className)}>
-        {children ||
-          options?.map(opt => (
-            <CheckboxOption value={opt.value} key={opt.value}>
-              {opt.label}
-            </CheckboxOption>
-          ))}
-      </div>
-    </Context.Provider>
+    <CheckboxBase onChange={handleChange} value={value} name={name} {...rest} />
   );
 }
-Checkbox.Option = CheckboxOption;
+Checkbox.Option = CheckboxBase.Option;
 export default Checkbox;
--- src/Radio/Radio.tsx
+++ src/Radio/Radio.tsx
@@ -1,91 +1,26 @@
 import React from 'react';
-import { twMerge } from 'tailwind-merge';
-import uniqueId from 'lodash/uniqueId';
 
-import FormButton from '../FormButton';
-import { PropsOf, AsProps, MutuallyExclude } from '../types';
+import CheckboxBase, { CheckboxBaseProps } from '../base/CheckboxBase';
 
-interface MutualStates {
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  value?: string | null;
-  name?: string;
-}
-
-const Context = React.createContext<MutualStates>({});
-
-export type FieldInputProps<BaseElement> = AsProps<BaseElement> &
-  Omit<PropsOf<BaseElement>, 'type' | 'onChange' | 'name'>;
-
-const DEFAULT_BASE = FormButton;
-function RadioOption<
-  BaseElement extends React.ElementType = typeof DEFAULT_BASE
->(props: FieldInputProps<BaseElement>) {
-  const { as, value: propsValue, ...rest } = props;
-  const { onChange, value, name } = React.useContext(Context);
-  return React.createElement(as || DEFAULT_BASE, {
-    // origin type with checkbox to ensure 'unselect' would be triggered
-    type: 'checkbox',
-    onChange,
-    checked: value === propsValue,
-    color: value && value !== propsValue ? 'secondary' : 'primary',
-    value: propsValue,
-    name,
-    ...rest,
-  });
-}
-
-interface Option {
-  value: string;
-  label: string;
-  sublabel?: string;
-}
-
-interface RadioProps {
-  options: Option[];
-  children: React.ReactNode;
+type RadioProps = CheckboxBaseProps<string> & {
   onChange?: (
     state: string | null,
-    event?:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.MouseEvent<HTMLInputElement>
+    event?: React.ChangeEvent<HTMLInputElement>
   ) => void;
-  value: string | null;
-  name?: string;
-  className?: string;
-}
-const defaultProps = {
-  name: uniqueId('radio-'),
-} as RadioProps;
-function Radio(
-  props: MutuallyExclude<
-    RadioProps & typeof defaultProps,
-    'options' | 'children'
-  >
-) {
-  const { options, value, onChange, children, name = '', className } = props;
-  const context = React.useMemo(
-    () => ({
-      name,
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        const v = e.currentTarget.value;
-        if (onChange) onChange(v === value ? null : v, e);
-      },
-      value,
-    }),
-    [name, onChange, value]
+};
+
+function Radio(props: RadioProps) {
+  const { value, onChange, name = '', ...rest } = props;
+  const handleChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const v = e.currentTarget.value;
+      if (onChange) onChange(v === value ? null : v, e);
+    },
+    [onChange, value]
   );
   return (
-    <Context.Provider value={context}>
-      <div className={twMerge('grid grid-cols-2 gap-2 mb-2', className)}>
-        {children ||
-          options?.map(opt => (
-            <RadioOption value={opt.value} key={opt.value}>
-              {opt.label}
-            </RadioOption>
-          ))}
-      </div>
-    </Context.Provider>
+    <CheckboxBase onChange={handleChange} value={value} name={name} {...rest} />
   );
 }
-Radio.Option = RadioOption;
+Radio.Option = CheckboxBase.Option;
 export default Radio;
